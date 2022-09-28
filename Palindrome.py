84f878b773017d95c96a2546676c76a80912c288
MADHAN151203 <96331328+MADHAN151203@users.noreply.github.com>
2022-09-28 15:25:27 +0530

    def reverse(n):     rev=0     temp=n     while n:         d=n%10         n=n//10         rev=rev*10+d     return rev==temp
    n=int(input())
    ans=reverse(n)
    print(ans)

---
+++ Palindrome.py
@@ -0,0 +1,18 @@
+def reverse(n):
+    rev=0
+    temp=n
+    while n:
+        d=n%10
+        n=n//10
+        rev=rev*10+d
+    return rev==temp
+
+
+
+
+
+
+
+n=int(input())
+ans=reverse(n)
+print(ans)
