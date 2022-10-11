0779b8811aedbcd3a9256da213d6479e840c7828
MADHAN151203 <96331328+MADHAN151203@users.noreply.github.com>
2022-10-11 15:46:04 +0530

    n=int(input()) a=0 b=1 if n==0:     print("True") c=a+b while c<n:     c=a+b     a=b     b=c if n==c:     print("True") else:     print("False")

--- Fibonacci_or_not.py
+++ Fibonacci_or_not.py
@@ -1,14 +1,14 @@
-x = int(input())
-a = 0
-b = 1
-if x==0:
+n=int(input())
+a=0
+b=1
+if n==0:
     print("True")
-c = a+b
-while c<x:
-    c = a+b
-    a = b 
-    b = c
-if x==c:
+c=a+b
+while c<n:
+    c=a+b
+    a=b
+    b=c
+if n==c:
     print("True")
 else:
     print("False")
