363db78cb22ad50c22a33fab98d505b527995656
MADHAN151203 <96331328+MADHAN151203@users.noreply.github.com>
2023-06-27 14:24:43 +0530

    n=int(input()) m1,m2,m3=[],[],[] for i in range(n):     m1.append(list(map(int,input().split()))) for i in range(n):     m2.append(list(map(int,input().split()))) for i in range(n):     l=[]     for j in range(n):         l.append(m1[i][j]+m2[i][j])     m3.append(l) for i in range(n):     for j in range(n):         print(m3[i][j],end=' ')     print()

---
+++ Addition_of_two_matrices.py
@@ -0,0 +1,15 @@
+n=int(input())
+m1,m2,m3=[],[],[]
+for i in range(n):
+    m1.append(list(map(int,input().split())))
+for i in range(n):
+    m2.append(list(map(int,input().split())))
+for i in range(n):
+    l=[]
+    for j in range(n):
+        l.append(m1[i][j]+m2[i][j])
+    m3.append(l)
+for i in range(n):
+    for j in range(n):
+        print(m3[i][j],end=' ')
+    print()
